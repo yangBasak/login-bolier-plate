7da31dcd558a95d47de62ec311ab9482fb855c32
yangBasak <yangBasak@gmail.com>
2022-04-23 11:09:11 +0900

    DB에 데이터 삽입중

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/user");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -14,8 +19,18 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
