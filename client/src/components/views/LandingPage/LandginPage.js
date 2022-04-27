466d002204e1f60c7df45564c3a655909a77004c
yangBasak <yangBasak@gmail.com>
2022-04-28 07:42:08 +0900

    리액트 설치 및 폴더 구조 변경

---
+++ client/src/components/views/LandingPage/LandginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LandingPage() {
+  return <div>랜딩</div>;
+}
+
+export default LandingPage;
---
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RegisterPage() {
+  return <div>레지스터</div>;
+}
+
+export default RegisterPage;
