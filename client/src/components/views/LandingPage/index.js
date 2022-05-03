dcb46c0fdafa5bf77b4e5ecf61c1dde803d41ab6
yangBasak <yangBasak@gmail.com>
2022-05-04 07:54:29 +0900

    랜딩페이지 약간, 로그인페이지 html구성 및 기능은 미완

--- client/src/components/views/LandingPage/index.js
+++ client/src/components/views/LandingPage/index.js
@@ -7,7 +7,11 @@ function LandingPage() {
       console.log(response);
     });
   });
-  return <div>랜d딩</div>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "100vh" }}>
+      <h2>시작 페이지</h2>
+    </div>
+  );
 }
 
 export default LandingPage;
--- client/src/components/views/LoginPage/index.js
+++ client/src/components/views/LoginPage/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage() {
-  return <div>로그인</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "100vh" }}>
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <label>이메일</label>
+        <input type="email" value={email} onChange={onEmailHandler} />
+        <label>비밀번호</label>
+        <input type="password" value={password} onChange={onPasswordHandler} />
+        <br />
+        <button>로그인</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
